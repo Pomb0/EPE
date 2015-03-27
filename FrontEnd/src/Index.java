339a0243eff2e6573ff129bfa1225d4b42c20de4
Jaime <jaime@slimecraft.pt>
2015-03-27 03:27:42 +0000

    Created FrontEnd module

---
+++ FrontEnd/src/Index.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+/**
+ * Created by Jaime on 27/03/2015.
+ */
+@javax.servlet.annotation.WebServlet(name = "Index")
+public class Index extends javax.servlet.http.HttpServlet {
+	protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+	}
+
+	protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+	}
+}
