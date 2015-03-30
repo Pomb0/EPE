5a78ee051dfc9191ee67d3fb3b555a39d68e36af
Jaime <jaime@slimecraft.pt>
2015-03-31 00:47:13 +0100

    Fixed Getters and setters and created UserBean

--- FrontEnd/src/Servlet/EPEServlet.java
+++ FrontEnd/src/Servlet/EPEServlet.java
@@ -3,7 +3,11 @@ package Servlet;
 import EJBInterface.UserEJBInterface;
 
 import javax.ejb.EJB;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by Jaime on 30/03/2015.
@@ -14,4 +18,20 @@ import javax.servlet.http.HttpServlet;
 public abstract class EPEServlet extends HttpServlet{
 	@EJB
 	private UserEJBInterface userEJB;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doPost(req, resp);
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doGet(req, resp);
+	}
+
+	//Stuff to be overridden.
+	protected abstract void onGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException;
+
+	protected abstract void onPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException;
+
 }
