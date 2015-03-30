205586fa5f498133037cdf7bd153342e624e8e13
Jaime <jaime@slimecraft.pt>
2015-03-31 00:04:16 +0100

    Added Data Beans

---
+++ FrontEnd/src/Servlet/EPEServlet.java
@@ -0,0 +1,12 @@
+package Servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by Jaime on 30/03/2015.
+ *
+ * Application specific Servlet abstraction.
+ * Takes care of session and notifications.
+ */
+public abstract class EPEServlet extends HttpServlet{
+}
