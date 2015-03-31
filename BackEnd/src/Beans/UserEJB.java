d816c8294f6e86ccbfb5e91f54a4f8e747989b2b
guilherme <g.baraosantos@gmail.com>
2015-03-31 06:36:24 +0100

    LogEJB

--- BackEnd/src/Beans/UserEJB.java
+++ BackEnd/src/Beans/UserEJB.java
@@ -1,17 +1,27 @@
 package Beans;
 
+import DataBean.OrderBean;
+import DataBean.UserBean;
 import EJBInterface.UserEJBInterface;
+import JPA.Entities.OrderEntity;
 import JPA.Entities.UserEntity;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.LinkedList;
+import java.util.List;
 
 
 @Stateless
 public class UserEJB implements UserEJBInterface {
     @PersistenceContext(name="jpaUnit")
     EntityManager entityManager;
+
+    public UserCreationResult createUser(String username, String password) {
+
+    }
 
     //Creates user, returns enum UserCreationResult
     @Override
@@ -33,5 +43,23 @@ public class UserEJB implements UserEJBInterface {
     @Override
     public boolean authenticate(String username, String password) {
         return true;
+    }
+
+    @Override
+    public UserBean getUser(int id) {
+        try {
+            Query query = entityManager.createQuery("FROM OrderEntity o WHERE o.id = :t");
+            query.setParameter("t", id);
+            UserEntity user = (UserEntity) query.getSingleResult();
+
+            UserBean Beanuser = user.toBean()
+
+            return Beanuser;
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
