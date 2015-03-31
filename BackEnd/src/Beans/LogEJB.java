08b5a973f2c5ce69fdb1cd3c23d5b01164a34715
Jaime <jaime@slimecraft.pt>
2015-03-31 03:44:46 +0100

    Cleared LogEJB due to being innapropriate

--- BackEnd/src/Beans/LogEJB.java
+++ BackEnd/src/Beans/LogEJB.java
@@ -3,55 +3,13 @@ package Beans;
 import EJBInterface.LogEJBInterface;
 
 import javax.ejb.Stateless;
-import javax.interceptor.Interceptors;
-import java.io.*;
+import java.io.IOException;
 
 @Stateless
 public class LogEJB implements LogEJBInterface{
 
-    private final String orderFilePath = "order.txt";
-    private final String logFilePath = "Logins.txt";
-    private final String shippingFilePath = "shipping.txt";
-
-    private static FileWriter writerOrder;
-    private static FileWriter writerLog;
-    private static FileWriter writerShipping;
-
-    public LogEJB(){
-        this.writerOrder = CreateWriter(orderFilePath);
-        this.writerLog = CreateWriter(logFilePath);
-        this.writerShipping = CreateWriter(shippingFilePath);
-
-    }
-
-    @Override
-    public FileWriter CreateWriter(String path) {
-        try{
-            File file = new File(path);
-
-            if(!file.exists()) { file.createNewFile(); }
-            return new FileWriter(file);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-       return null;
-    }
-
     @Override
     public void addLog(String log, int type) throws IOException {
-        switch (type){
-            case(0):
-                this.writerLog.append(log + "\n");
-                break;
-            case(1):
-                this.writerLog.append(log + "\n");
-                break;
-            case(2):
-                this.writerShipping.append(log + "\n");
-                break;
-            default:
-                break;
-        }
 
     }
 }
